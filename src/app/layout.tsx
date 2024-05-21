8a9ca37e50caad8e873df61aeec42a4839099f51
Francesco Scutellaro <dev.mirror42@gmail.com>
2024-05-21 23:59:01 +0200

    feat(app): add navigation bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,14 @@ export const metadata: Metadata = {
   },
 };
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  // maximumScale: 1,
+  // userScalable: false,
+  viewportFit: "cover",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -30,22 +38,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <div className="h-full">{children}</div>
+        {children}
 
-        <nav className={styles.navigation}>
-          <ul className={styles.navigation__items}>
-            <li className={styles.navigation__item}>
-              <Link href="/" className={styles.navigation__link}>
-                <FontAwesomeIcon icon={faHome} />
-              </Link>
-            </li>
-            <li className={styles.navigation__item}>
-              <Link href="/about" className={styles.navigation__link}>
-                <FontAwesomeIcon icon={faInfo} />
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        {/*<nav className={styles.navigation}>*/}
+        {/*  <ul className={styles.navigation__items}>*/}
+        {/*    <li className={styles.navigation__item}>*/}
+        {/*      <Link href="/" className={styles.navigation__link}>*/}
+        {/*        <FontAwesomeIcon icon={faHome} />*/}
+        {/*      </Link>*/}
+        {/*    </li>*/}
+        {/*    <li className={styles.navigation__item}>*/}
+        {/*      <Link href="/about" className={styles.navigation__link}>*/}
+        {/*        <FontAwesomeIcon icon={faInfo} />*/}
+        {/*      </Link>*/}
+        {/*    </li>*/}
+        {/*  </ul>*/}
+        {/*</nav>*/}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import styles from "./App.module.scss";
 
 export default function Home() {
   return (
-    <main className="m-3 h-dvh">
+    <main className="m-3">
       <Timeline />
     </main>
   );
