a24bef027334670affe1fa2eac5801680fc7642e
Francesco Scutellaro <dev.mirror42@gmail.com>
2024-05-21 23:29:46 +0200

    feat(app): add navigation bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export const metadata: Metadata = {
     capable: true,
     statusBarStyle: "black-translucent",
   },
+  other: {
+    "mobile-web-app-capable": "yes",
+  },
 };
 
 export default function RootLayout({
