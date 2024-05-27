f475fa27ba66e511918c01ee6eb876e1f2bc2671
Francesco Scutellaro <dev.mirror42@gmail.com>
2024-05-27 22:53:19 +0200

    feat(app): add add item modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  // maximumScale: 1,
+  maximumScale: 1,
   // userScalable: false,
   viewportFit: "cover",
 };
